bf22bca7bd4b8eb8f79cf140fe4c0d592f1c6475
Amit kumar <amit68568@gmail.com>
2021-09-05 19:23:55 +0530

    java file add

---
+++ javas/first.java
@@ -0,0 +1,9 @@
+import java.io.*;
+import java.util.*;
+public class first {
+    public static void main(String args[]) {
+        // your code here
+        int a;
+        System.out.println(a);
+    }
+}
